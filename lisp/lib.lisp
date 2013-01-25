875fdfbf2eef77a6f95a81404db46cea7f0e42f5
Tristan Straub <tristanstraub@gmail.com>
2013-01-25 17:30:39 +1000

    reformatting

--- lisp/lib.lisp
+++ lisp/lib.lisp
@@ -1,13 +1,13 @@
 (defmacro and (a b)
   (list 'if a
-				(list 'if b
-							't)))
+        (list 'if b
+              't)))
 
 (defmacro progn (&rest body)
-	(list (quote funcall) (cons (quote lambda) (cons (quote ()) body))))
+  (list (quote funcall) (cons (quote lambda) (cons (quote ()) body))))
 
 (defun caar (x)
-	(car (car x)))
+  (car (car x)))
 
 (defun cadr (x)
   (car (cdr x)))
@@ -22,25 +22,25 @@
   (car (cdr (cddr x))))
 
 (defun cdddr (x)
-	(cdr (cdr (cdr x))))
+  (cdr (cdr (cdr x))))
 
 (defmacro let-1 (pars body)
   (cons 
    (quote funcall)
    (cons (cons 
-					(quote lambda)
-					(cons (cons (car pars) nil) body))
-				 (cons (cadr pars) nil))))
+          (quote lambda)
+          (cons (cons (car pars) nil) body))
+         (cons (cadr pars) nil))))
 
 (defmacro let* (pars &rest body)
   (if pars
       (list (quote let-1)
-						(car pars)
-						(if (cdr pars)
-								(list (cons (quote let*) (cons (cdr pars)
-																							 body)))
-								body))
-			body))
+            (car pars)
+            (if (cdr pars)
+                (list (cons (quote let*) (cons (cdr pars)
+                                               body)))
+                body))
+      body))
 
 (defun cond% (rest)
   (let* 
@@ -64,7 +64,7 @@
   (cond ((not x)
          nil)
         ((atom x)
-				 (list 'quote x))
+         (list 'quote x))
         ((eql 'uq (car x))
          (cadr x))
         ((and (and (listp (car x))
@@ -80,21 +80,21 @@
                (list 'qq (cdr x))))))
 
 (defun requires* (these callback files)
-	(if these
-			(require (car these)
-							 (lambda (data)
-								 (requires* (cdr these) 
-													 callback 
-													 (cons (car these) (cons data files)))))
-			(if callback
-					(funcall callback files))))
+  (if these
+      (require (car these)
+               (lambda (data)
+                 (requires* (cdr these) 
+                            callback 
+                            (cons (car these) (cons data files)))))
+      (if callback
+          (funcall callback files))))
 
 (defun requires (these callback)
-	(requires* these callback nil))
+  (requires* these callback nil))
 
 (defun println (x)
-	(print x)
-	(print "\n"))
+  (print x)
+  (print "\n"))
 
 (defun list (&rest rest)
   rest)
@@ -102,7 +102,7 @@
 (defun not (x)
   (if x
       nil
-			t))
+      t))
 
 (defun listp (x)
   (not (atom x)))
@@ -110,14 +110,14 @@
 (defmacro or (a b)
   `(if ,a
        t
-			 (if ,b
-					 t)))
+       (if ,b
+           t)))
 
 (defun in (el hs)
   (if hs
       (if (eql el (car hs))
-					t
-					(in el (cdr hs)))))
+          t
+          (in el (cdr hs)))))
 
 (defun assoc (el list)
   (if list
@@ -128,50 +128,50 @@
 (defun mapcar (fun list)
   (if list
       (cons (funcall fun (car list))
-						(mapcar fun (cdr list)))))
+            (mapcar fun (cdr list)))))
 
 (defun mapicar (fun list &rest index)
   (let* ((i 
           (if index
               (car index)
-							0)))
+              0)))
     (if list
         (cons (funcall fun i (car list))
               (mapicar fun (cdr list) (+ i 1))))))
 
 (defun last (values)
   (let* ((ret 
-					(if (cdr values)
-							(last (cdr values))
-							(car values))))
+          (if (cdr values)
+              (last (cdr values))
+              (car values))))
     ret))
 
 (defun heads (values)
-	(if (cdr values)
-			(cons (car values) (heads (cdr values)))))
+  (if (cdr values)
+      (cons (car values) (heads (cdr values)))))
 
 (defun r (c)
-	(cond ((null c)
-				 c)
-				((atom c)
-				 c)
-				(t
-				 (cons (r (cdr c)) (r (car c))))))
+  (cond ((null c)
+         c)
+        ((atom c)
+         c)
+        (t
+         (cons (r (cdr c)) (r (car c))))))
 
 (defun append (heads tail)
-	(if heads
-			(cons (car heads)
-						(append (cdr heads) tail))
-			tail))
+  (if heads
+      (cons (car heads)
+            (append (cdr heads) tail))
+      tail))
 
 (defun reverse (values)
-	(if values
-			(append (reverse (cdr values)) (list (car values)))))
+  (if values
+      (append (reverse (cdr values)) (list (car values)))))
 
 (defun append-each (heads tails)
-	(if tails
-			(append-each (append heads (car tails)) (list (cdr tails)))
-			heads))
+  (if tails
+      (append-each (append heads (car tails)) (list (cdr tails)))
+      heads))
 
 (defun elt (seq i)
   (car (subseq seq i (+ i 1))))
@@ -182,21 +182,21 @@
        (string-t (pretty-print-to-string (car form)))
        (if (cdr form)
            (concat " " (string-t (pretty-print-list (cdr form))))))
-			"nil"))
+      "nil"))
 
 
 
 (defun pretty-print-to-string (form)
-	(cond ((null form) "nil")
-				((string-t-p form)
-				 (concat "\"" form "\""))
-				((atom form)
-				 (string-t form))
-				(t
-				 (concat "(" (pretty-print-list form) ")"))))
+  (cond ((null form) "nil")
+        ((string-t-p form)
+         (concat "\"" form "\""))
+        ((atom form)
+         (string-t form))
+        (t
+         (concat "(" (pretty-print-list form) ")"))))
 
 (defun repr (form)
-	(pretty-print-to-string form))
+  (pretty-print-to-string form))
 
 (defun print-list (values)
   (if values
@@ -211,13 +211,13 @@
   `(let* ((test-result ,test))
      (if (equal test-result ,result)
          (println (concat "passed: " (repr ',test) "->" (repr test-result) "==" (repr ,result)))
-				 (println (concat "FAILED: " (repr ',test) "->" (repr test-result) "!=" (repr ,result))))))
+         (println (concat "FAILED: " (repr ',test) "->" (repr test-result) "!=" (repr ,result))))))
 
 (defmacro test-not-equal (test result)
   `(let* ((test-result ,test))
      (if (not (equal test-result ,result))
          (println (concat "passed: " (repr ',test) "->" (repr test-result) "!=" (repr ,result)))
-				 (println (concat "FAILED: " (repr ',test) "->" (repr test-result) "==" (repr ,result))))))
+         (println (concat "FAILED: " (repr ',test) "->" (repr test-result) "==" (repr ,result))))))
 
 (defmacro test-assert (test)
   `(test-equal ,test t))
@@ -225,18 +225,18 @@
 (defun equal (a b)
   (if (and (not a) (not b))
       t
-			(if (and a (not b))
-					nil
-					(if (and b (not a))
-							nil
-							(if (and (atom a) (not (atom b)))
-									nil
-									(if (and (not (atom a)) (atom b))
-											nil
-											(if (atom a)
-													(eql a b)
-													(and (equal (car a) (car b))
-															 (equal (cdr a) (cdr b))))))))))
+      (if (and a (not b))
+          nil
+          (if (and b (not a))
+              nil
+              (if (and (atom a) (not (atom b)))
+                  nil
+                  (if (and (not (atom a)) (atom b))
+                      nil
+                      (if (atom a)
+                          (eql a b)
+                          (and (equal (car a) (car b))
+                               (equal (cdr a) (cdr b))))))))))
 
 
 (defun remove-if-not (f list)
@@ -244,10 +244,10 @@
       (let* ((rest (remove-if-not f (cdr list))))
         (if (funcall f (car list))
             (cons (car list) rest)
-						rest))))
+            rest))))
 
 (defun remove-if (f list)
-	(remove-if-not (lambda (x) (not (funcall f x))) list))
+  (remove-if-not (lambda (x) (not (funcall f x))) list))
 
 (defun weave (x y)
   (if x
@@ -267,58 +267,58 @@
       (concat
        (car hs) 
        (if (cdr hs)
-					 (concat sep (join (cdr hs) sep))
-					 ""))
-			""))
+           (concat sep (join (cdr hs) sep))
+           ""))
+      ""))
 
 (defun reduce (fun accumulator hs)
   (if hs
       (reduce fun
-							(funcall fun accumulator (car hs))
-							(cdr hs))
-			accumulator))
+              (funcall fun accumulator (car hs))
+              (cdr hs))
+      accumulator))
 
 (defun last? (xs)
-	(and xs
-			 (null (cdr xs))))
+  (and xs
+       (null (cdr xs))))
 
 (defun except-last (xs)
-	(if (last? xs)
-			nil
-			(cons (car xs) (except-last (cdr xs)))))
+  (if (last? xs)
+      nil
+      (cons (car xs) (except-last (cdr xs)))))
 
 (defun string-escape (form)
   (if form
       (let* ((form (string-t form)))
-				(if (eql form "")
-						""
-						(let* ((c (subseq form 0 1))
-									 (rest (subseq form 1)))
-							(concat
-							 (cond ((eql c "'")
-											"\\'")
-										 ((eql c "\"")
-											"\\\"")
-										 ((eql c "\\")
-											"\\\\")
-										 ((eql c "\n")
-											"\\n")
-										 ((eql c "\r")
-											"\\r")
-										 ((eql c "\t")
-											"\\t")
-										 (t c))
-							 (string-escape rest)))))
+        (if (eql form "")
+            ""
+            (let* ((c (subseq form 0 1))
+                   (rest (subseq form 1)))
+              (concat
+               (cond ((eql c "'")
+                      "\\'")
+                     ((eql c "\"")
+                      "\\\"")
+                     ((eql c "\\")
+                      "\\\\")
+                     ((eql c "\n")
+                      "\\n")
+                     ((eql c "\r")
+                      "\\r")
+                     ((eql c "\t")
+                      "\\t")
+                     (t c))
+               (string-escape rest)))))
       ""))
 
 (defmacro let (vars &rest body)
-	`(let* ,vars ,@body))
+  `(let* ,vars ,@body))
 
 (defmacro case (value &rest rest)
-	(if rest
-			(let ((first (car rest)))
-				`(cond ((equal ,value ',(car first)) 
-								,@(cdr first))
-							 (t 
-								(case ,value ,@(cdr rest)))))))
+  (if rest
+      (let ((first (car rest)))
+        `(cond ((equal ,value ',(car first)) 
+                ,@(cdr first))
+               (t 
+                (case ,value ,@(cdr rest)))))))
 
--- lisp/vops.lisp
+++ lisp/vops.lisp
@@ -2,39 +2,39 @@
   1)
 
 (defun nextenv (env)
-	(if (null env)
-			1
-			(+ env 1)))
+  (if (null env)
+      1
+      (+ env 1)))
 
 (defun prevenv (env)
-	(if (eql env 1)
-			nil
-			(- env 1)))
+  (if (eql env 1)
+      nil
+      (- env 1)))
 
 (defun denv (env)
-	(concat "env" env))
+  (concat "env" env))
 
 (defmacro defvop (name args &rest body)
-	`(defun ,(symbol-t (concat "vop:" (string-t name)))
-			 ,args
-		 ,@body))
+  `(defun ,(symbol-t (concat "vop:" (string-t name)))
+       ,args
+     ,@body))
 
 (defun vopsquote-unquote (forms)
-	(mapcar (lambda (x)
-						(cond ((and (listp x)
-												(eql (car x) 'vopsquote-uq))
-									 (compile (cadr x) nil))
-									((listp x)
-									 (vopsquote-unquote x))
-									(t x)))
-					forms))
+  (mapcar (lambda (x)
+            (cond ((and (listp x)
+                        (eql (car x) 'vopsquote-uq))
+                   (compile (cadr x) nil))
+                  ((listp x)
+                   (vopsquote-unquote x))
+                  (t x)))
+          forms))
 
 (defmacro vopsquote (forms)
-	`(vops (concat ,@(vopsquote-unquote forms))))
+  `(vops (concat ,@(vopsquote-unquote forms))))
 
 (defvop init ()
-	(concat
-	 "if (typeof window !== 'undefined')
+  (concat
+   "if (typeof window !== 'undefined')
 		{
 				modules = {
 						fs : {
@@ -53,187 +53,187 @@
 				
 				process = { argv : [] };
 		}"
-	 "var sys=require('sys');"
-	 "var fs=require('fs');"
-	 "if (typeof functions === 'undefined') {"
-	 "    functions = {};"
-	 "}"
-	 "if (typeof macros === 'undefined') {"
-	 "    macros = {};"
-	 "}"
-	 "function Symbol(value)
+   "var sys=require('sys');"
+   "var fs=require('fs');"
+   "if (typeof functions === 'undefined') {"
+   "    functions = {};"
+   "}"
+   "if (typeof macros === 'undefined') {"
+   "    macros = {};"
+   "}"
+   "function Symbol(value)
 				 {
 						 this.value = value;
 				 };"))
 
 (defvop t ()
-	"true")
+  "true")
 
 (defvop nil ()
-	"null")
+  "null")
 
 (defun undefined->null (x)
-	~("((" ^x "!=null && typeof " ^x " !== 'undefined')?" ^x ":null)"))
+  ~("((" ^x "!=null && typeof " ^x " !== 'undefined')?" ^x ":null)"))
 
 (defvop if (if then else env)
-	(concat "((" 
-					(compile `(undefined->null ,if) env) " != null)?(" 
-					(compile then env) "):("
-					(compile else env) "))"))
+  (concat "((" 
+          (compile `(undefined->null ,if) env) " != null)?(" 
+          (compile then env) "):("
+          (compile else env) "))"))
 
 (defmacro typeof (arg)
-	`~("(typeof " ^,arg ")"))
+  `~("(typeof " ^,arg ")"))
 
 (defmacro top-var (var env)
-	(if env
-			`(compile `(if (=== (typeof (vops (vop:lookup ,var ,env))) "undefined")
-										 (top-var ,var ,(prevenv env))
-										 ~("undefined"))
-								,env)
-						nil))
+  (if env
+      `(compile `(if (=== (typeof (vops (vop:lookup ,var ,env))) "undefined")
+                     (top-var ,var ,(prevenv env))
+                     ~("undefined"))
+                ,env)
+      nil))
 
 (defvop assign (var val env)
-	(concat (compile var env)
-					"=" 
-					(compile val env)))
+  (concat (compile var env)
+          "=" 
+          (compile val env)))
 
 (defun quoted-symbol-p (k)
-	(and (listp k)
-			 (and (eql (car k) 'quote)
-						(symbol-t-p (cadr k)))))
+  (and (listp k)
+       (and (eql (car k) 'quote)
+            (symbol-t-p (cadr k)))))
 
 (defun string->hexencode (str)
-	(if (> (array.length str) 0)
-			(let* ((hex "0123456789abcdef")
-						 (val (char-code (aref str 0))))
-				(concat (aref hex (math.floor (/ val 16)))
-								(aref hex (% val 16))
-								(string->hexencode (subseq str 1))))
-			""))
+  (if (> (array.length str) 0)
+      (let* ((hex "0123456789abcdef")
+             (val (char-code (aref str 0))))
+        (concat (aref hex (math.floor (/ val 16)))
+                (aref hex (% val 16))
+                (string->hexencode (subseq str 1))))
+      ""))
 
 (defun make-fn-name (form)
-	(concat "fn_" (string->hexencode (string-t (cadr form)))))
+  (concat "fn_" (string->hexencode (string-t (cadr form)))))
 
 (defun make-var-name (form)
-	(concat "var_" (string->hexencode (string-t form))))
+  (concat "var_" (string->hexencode (string-t form))))
 
 (defvop lookup (form env)
-	(make-var-name form))
+  (make-var-name form))
 
 (defvop load-from (where name env)
-	(let* ((rename-symbol (lambda (name)
-													(if (quoted-symbol-p name)
-															(concat "'" (string-t (cadr name)) "'")
-															(concat (compile `(string-t ,name) nil))))))
-		(cond ((eql where 'functions-dict)
-					 (concat "functions[" (funcall rename-symbol name) "]"))
-					((and (eql where 'functions)
-								(quoted-symbol-p name))
-					 (make-fn-name name))
-					((eql where 'env)
-					 (vop:lookup name env))
-					(where 
-					 (concat (string-t where) "[" (funcall rename-symbol name) "]"))
-					(t
-					 (concat (string-t (cadr name)))))))
+  (let* ((rename-symbol (lambda (name)
+                          (if (quoted-symbol-p name)
+                              (concat "'" (string-t (cadr name)) "'")
+                              (concat (compile `(string-t ,name) nil))))))
+    (cond ((eql where 'functions-dict)
+           (concat "functions[" (funcall rename-symbol name) "]"))
+          ((and (eql where 'functions)
+                (quoted-symbol-p name))
+           (make-fn-name name))
+          ((eql where 'env)
+           (vop:lookup name env))
+          (where 
+           (concat (string-t where) "[" (funcall rename-symbol name) "]"))
+          (t
+           (concat (string-t (cadr name)))))))
 
 
 (defvop defun (form env)
-	(:compile-progn 
-	 (make-forms-reader `((setq (location 'functions-dict ',(cadr form)) (lambda ,(caddr form) ,@(cdddr form)))
-												(setq (local ',(symbol-t (make-fn-name form)))
-															(location 'functions-dict ',(cadr form)))))
-	 env
-	 (lambda (x) x)))
+  (:compile-progn 
+   (make-forms-reader `((setq (location 'functions-dict ',(cadr form)) (lambda ,(caddr form) ,@(cdddr form)))
+                        (setq (local ',(symbol-t (make-fn-name form)))
+                              (location 'functions-dict ',(cadr form)))))
+   env
+   (lambda (x) x)))
 
 (defvop symbol (form env)
-	(concat "(new Symbol('" (string-escape (string-t form)) "'))"))
+  (concat "(new Symbol('" (string-escape (string-t form)) "'))"))
 
 (defvop number (form)
-	form)
+  form)
 
 (defvop string (form)
-	(concat "'" (string-escape form) "'"))
+  (concat "'" (string-escape form) "'"))
 
 (defvop quote (form env)
-	(cond ((not form)                           (vop:nil))
-				((atom form) (cond ((number-t-p form) (vop:number form))
-													 ((string-t-p form) (vop:string form))
-													 (t                 (vop:symbol form env))))
-				(t                        (concat "["
-																					(vop:quote (car form) env)
-																					","
-																					(vop:quote (cdr form) env)
-																					"]"))))
+  (cond ((not form)                           (vop:nil))
+        ((atom form) (cond ((number-t-p form) (vop:number form))
+                           ((string-t-p form) (vop:string form))
+                           (t                 (vop:symbol form env))))
+        (t                        (concat "["
+                                          (vop:quote (car form) env)
+                                          ","
+                                          (vop:quote (cdr form) env)
+                                          "]"))))
 
 
 (defun compiledforms->commalist (xs env)
-	(if (null xs)
-			(vop:nil)
-			(concat "[" 
-							(compile (car xs) env)
-							"," 
-							(compiledforms->commalist (cdr xs) env)
-							"]")))
+  (if (null xs)
+      (vop:nil)
+      (concat "[" 
+              (compile (car xs) env)
+              "," 
+              (compiledforms->commalist (cdr xs) env)
+              "]")))
 
 (defvop list (xs)
-	(if (null xs)
-			(vop:nil)
-			(concat "[" 
-							(car xs)
-							"," 
-							(vop:list (cdr xs))
-							"]")))
+  (if (null xs)
+      (vop:nil)
+      (concat "[" 
+              (car xs)
+              "," 
+              (vop:list (cdr xs))
+              "]")))
 
 (defvop block (xs)
-	(if (null xs)
-			""
-			(concat (car xs)
-							";\n"
-							(vop:block (cdr xs)))))
+  (if (null xs)
+      ""
+      (concat (car xs)
+              ";\n"
+              (vop:block (cdr xs)))))
 
 (defvop call (fn args env)
-	(concat (compile fn env) 
-					"(" 
-					(compiledforms->commalist args env)
-					")"))
+  (concat (compile fn env) 
+          "(" 
+          (compiledforms->commalist args env)
+          ")"))
 
 (defvop apply (fn args env)
-	(concat (compile fn env) 
-					"(" 
-					(compile args env)
-					")"))
+  (concat (compile fn env) 
+          "(" 
+          (compile args env)
+          ")"))
 
 (defun compile-group-parameters (names env)
   (if names
       (cond ((eql (car names) '&rest)
-						 (concat "\t" 
-										 "var " (compile (cadr names) env) "=args;\n"))
-						(t
-						 (concat "\t" 
-										 "var " (compile (car names) env) "=args[0];\n"
-										 "args=args[1];\n"
-										 (compile-group-parameters (cdr names) env))))))
+             (concat "\t" 
+                     "var " (compile (cadr names) env) "=args;\n"))
+            (t
+             (concat "\t" 
+                     "var " (compile (car names) env) "=args[0];\n"
+                     "args=args[1];\n"
+                     (compile-group-parameters (cdr names) env))))))
 
 (defvop return (form env)
-	(concat "return " (compile form env) ";"))
+  (concat "return " (compile form env) ";"))
 
 (defvop closure (form env)
-	(let* ((lenv (nextenv env)))
-		(concat "(function(args) {\n"
-						(compile-group-parameters (cadr form) lenv)
-						(if (cddr form)
-								(:compile-progn (make-forms-reader (cddr form)) lenv (lambda (x) (concat "\treturn " x ";")))
-								"\treturn null;\n")
-						"})\n")))
+  (let* ((lenv (nextenv env)))
+    (concat "(function(args) {\n"
+            (compile-group-parameters (cadr form) lenv)
+            (if (cddr form)
+                (:compile-progn (make-forms-reader (cddr form)) lenv (lambda (x) (concat "\treturn " x ";")))
+                "\treturn null;\n")
+            "})\n")))
 
 (defmacro defboolbinop (name)
-	`(defmacro ,name (a b)
-		 `~("((" ^,',a ,(string-t name) ^,',b ")?true:null)")))
+  `(defmacro ,name (a b)
+     `~("((" ^,',a ,(string-t name) ^,',b ")?true:null)")))
 
 (defmacro defbinop (name)
-	`(defmacro ,name (a b)
-		 `~("(" ^,',a ,(string-t name) ^,',b ")")))
+  `(defmacro ,name (a b)
+     `~("(" ^,',a ,(string-t name) ^,',b ")")))
 
 (defboolbinop >)
 (defboolbinop <)
@@ -249,120 +249,120 @@
 (defbinop %)
 
 (defun cons (a b)
-	~("[" ^a "," ^b "]"))
+  ~("[" ^a "," ^b "]"))
 
 (defmacro cons (a b)
-	`~("[" ^,a "," ^,b "]"))
+  `~("[" ^,a "," ^,b "]"))
 
 (defun car (x)
-	~(^x "[0]"))
+  ~(^x "[0]"))
 
 (defmacro car (x)
-	`~(^,x "[0]"))
+  `~(^,x "[0]"))
 
 (defun cdr (x)
-	~(^x "[1]"))
+  ~(^x "[1]"))
 
 (defmacro cdr (x)
-	`~(^,x "[1]"))
+  `~(^,x "[1]"))
 
 (defun null (x)
-	(or (eql x nil)
-			(eql (typeof x) "undefined")))
+  (or (eql x nil)
+      (eql (typeof x) "undefined")))
 
 (defun fatal ()
-	"throw 'fatal error';")
+  "throw 'fatal error';")
 
 (defun meval (code)
-	~("try {")
-	~("return eval(" ^code ")")
-	~("} catch(e) { sys.debug(" ^code "); throw e; }")
-	nil)
+  ~("try {")
+  ~("return eval(" ^code ")")
+  ~("} catch(e) { sys.debug(" ^code "); throw e; }")
+  nil)
 
 (defun printerror (arg)
-	~("sys.debug(" ^arg ")")
-	arg)
+  ~("sys.debug(" ^arg ")")
+  arg)
 
 (defun symbol-t-p (arg)
-	~("(" ^arg " instanceof Symbol ? true:null)"))
+  ~("(" ^arg " instanceof Symbol ? true:null)"))
 
 (defun string-t-p (arg)
-	~("(typeof " ^arg " === 'string' ? true:null)"))
+  ~("(typeof " ^arg " === 'string' ? true:null)"))
 
 (defun number-t-p (arg)
-	~("(typeof " ^arg " === 'number' ? true:null)"))
+  ~("(typeof " ^arg " === 'number' ? true:null)"))
 
 (defun symbol-t (arg)
-	(if (symbol-t-p arg)
-			~("new Symbol(" ^arg ".value)")
-			~("new Symbol(''+" ^arg ")")))
+  (if (symbol-t-p arg)
+      ~("new Symbol(" ^arg ".value)")
+      ~("new Symbol(''+" ^arg ")")))
 
 (defun boolean-t-p (arg)
-	~("(typeof " ^arg " === 'boolean' ? true:null)"))
+  ~("(typeof " ^arg " === 'boolean' ? true:null)"))
 
 (defun string-t (arg)
-	(cond ((symbol-t-p arg)
-				 ~(^arg ".value"))
-				((boolean-t-p arg)
-				 ~("(" ^arg "?'t':'nil')"))
-				(t
-				 ~("''+" ^arg))))
+  (cond ((symbol-t-p arg)
+         ~(^arg ".value"))
+        ((boolean-t-p arg)
+         ~("(" ^arg "?'t':'nil')"))
+        (t
+         ~("''+" ^arg))))
 
 
 (defun number-t (arg)
-	(if (symbol-t-p arg)
-			~("parseFloat(" ^arg ".value)")
-			~("parseFloat(''+" ^arg ")")))
+  (if (symbol-t-p arg)
+      ~("parseFloat(" ^arg ".value)")
+      ~("parseFloat(''+" ^arg ")")))
 
 (defun eql (x y)
-	(cond ((and (symbol-t-p x)
-							(symbol-t-p y))
-				 ~("(" ^x ".value == " ^y ".value ? true:null)"))
-				(t
-				 ~("(" ^x " == " ^y "?true:null)"))))
+  (cond ((and (symbol-t-p x)
+              (symbol-t-p y))
+         ~("(" ^x ".value == " ^y ".value ? true:null)"))
+        (t
+         ~("(" ^x " == " ^y "?true:null)"))))
 
 (defun atom (arg)
-	(cond ((null arg) t)
-				((symbol-t-p arg) t)
-				(t 
-				 (in (typeof arg) '("boolean" "number" "string")))))
+  (cond ((null arg) t)
+        ((symbol-t-p arg) t)
+        (t 
+         (in (typeof arg) '("boolean" "number" "string")))))
 
 (defun concat ()
-	~("var s = '';
+  ~("var s = '';
 	while(args != null)
 	{
 	if (args[0] != null)
 	s += args[0];
 	args = args[1];
 	}")
-	~("s"))
+  ~("s"))
 
 (defun read-line (callback)
-		~("process.stdin.resume();
+  ~("process.stdin.resume();
      process.stdin.once('data',
      function(data) {
        " ^callback "([data+'', null]);
      });")
-	nil)
+  nil)
 
 (defun require ()
-	~("var file = args[0];
+  ~("var file = args[0];
 	   var callback = args[1][0];
 
 		 fs.readFile(file, function(err, data) {
 			 callback([data+'', null]);
 		 });")
-	nil)
+  nil)
 
 (defun subseq ()
-	~("if (args[1][1] != null)
+  ~("if (args[1][1] != null)
 			 return args[0].substring(args[1][0], args[1][1][0]);
 		 else
 			 return args[0].substring(args[1][0]);")
-	nil)
+  nil)
 
 (defun print ()
-	~("var values = [];
+  ~("var values = [];
 		 var printvalues = [];
 		 var v = null;
 		 while(args != null)
@@ -376,52 +376,52 @@
      }
      sys.print(printvalues.join(''));
      return v;")
-	nil)
+  nil)
 
 (defun array.length (array)
-	~(^array ".length"))
+  ~(^array ".length"))
 
 (defun array->list (array)
-	(if (> (array.length array) 0)
-			(cons ~(^array "[0]")
-						(if (> (array.length array) 1)
-								(array->list ~(^array ".slice(1)"))))))
+  (if (> (array.length array) 0)
+      (cons ~(^array "[0]")
+            (if (> (array.length array) 1)
+                (array->list ~(^array ".slice(1)"))))))
 
 (defun aref (array i)
-	~(^array "[" ^i "]"))
+  ~(^array "[" ^i "]"))
 
 (defun char-code (s)
-	~(^s ".charCodeAt(0)"))
+  ~(^s ".charCodeAt(0)"))
 
 (defun math.floor (num)
-	~("Math.floor(" ^num ")"))
+  ~("Math.floor(" ^num ")"))
 
 (defmacro make-hash ()
-	'~("{}"))
+  '~("{}"))
 
 (defmacro get-hash (hash key)
-	`~(^,hash "[" ^(string-t ,key) "]"))
+  `~(^,hash "[" ^(string-t ,key) "]"))
 
 (defmacro set-hash (hash key value)
-	`(setq ~(^,hash "[" ^(string-t ,key) "]") ,value))
+  `(setq ~(^,hash "[" ^(string-t ,key) "]") ,value))
 
 (defmacro length (str)
-	`~(^,str ".length"))
+  `~(^,str ".length"))
 
 (defmacro string.length (str)
-	`~(^,str ".length"))
+  `~(^,str ".length"))
 
 (defmacro catch-errors (fn &rest body)
-	`(progn ~("try {" ^,@body "} catch(e) { " ^(funcall ,fn ~("e")) "}")
-					nil))
+  `(progn ~("try {" ^,@body "} catch(e) { " ^(funcall ,fn ~("e")) "}")
+          nil))
 
 (defmacro labels (functions &rest body)
-	`(apply (lambda () 
-							~(,@(mapcar (lambda (fn) `^~("var " ^(setq (function ',(car fn))
-																											 (lambda ,(cadr fn) ,@(cddr fn)))
-																								";"))
-												functions))
-							,@body)
-					nil))
+  `(apply (lambda () 
+            ~(,@(mapcar (lambda (fn) `^~("var " ^(setq (function ',(car fn))
+                                                       (lambda ,(cadr fn) ,@(cddr fn)))
+                                                ";"))
+                        functions))
+            ,@body)
+          nil))
 
 
